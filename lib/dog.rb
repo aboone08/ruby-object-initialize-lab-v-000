a0774a94c4832c8d2e8b648afce22d5218fe2d4f
Andranique Boone <andranique.boone@gmail.com>
2019-01-14 00:32:30 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,10 @@
 class Dog 
   def initialize (dog_name, breed="morkie")
-    @dog_name = dog_name
+    @name = dog_name
     @breed = breed 
   end 
   def dog_name
-    @dog_name
+    @name
   end
   def breed 
     @breed 
